a7f3572827c179c29bf3c369f71c8d8d2ffba6ac
IMperiumX <yusufadell.dev@gmail.com>
2025-01-23 22:31:11 +0200

    feat: initialize common app structure with models, views, and admin registration

---
+++ quizify/common/decorators.py
@@ -0,0 +1,7 @@
+"""
+TODO
+Delay execution of the function, within ttl seconds, only the last call is executed
+:param ttl: Time to live in seconds
+:param key: Whether to merge parameters, a callback
+:return: Decorator function
+"""
---
+++ quizify/common/utils/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F403
+from .common import *
+from .django import *
+from .encode import *
+from .random import *
